09c4471ecd1831aee24e1e3189f17af990f92968
Anisa Hertriani <49422884+hertrianianisa@users.noreply.github.com>
2019-04-09 06:38:12 -0700

    Delete function.js

--- JS/function.js
+++
@@ -1,7 +0,0 @@
-<script type="text/javascript" src="path-to-javascript-file.js"></script>
-/* Demo purposes only */
-$(".hover").mouseleave(
-    function () {
-      $(this).removeClass("hover");
-    }
-  );
